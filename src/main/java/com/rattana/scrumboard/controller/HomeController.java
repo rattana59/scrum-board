ab237fd0bdbd15797a80a7ef94701ca36c1cba70
jintasit <jintasit.yot@gmail.com>
2022-06-10 21:15:26 +0700

    Added devtool for auto restart develop server

--- src/main/java/com/rattana/scrumboard/controller/HomeController.java
+++ src/main/java/com/rattana/scrumboard/controller/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Spring Boot!!!  ";
+        return "Hello Spring Boot!!! \\(^O^)/";
     }
 }
