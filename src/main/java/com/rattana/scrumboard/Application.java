04dbfbb5fad42cf7b2bf6f4e609101e5c1a268ce
Jintasit <jintasit.yot@gmail.com>
2022-06-18 15:43:39 +0700

    Connected application to database using docker

--- src/main/java/com/rattana/scrumboard/Application.java
+++ src/main/java/com/rattana/scrumboard/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScrumboardApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScrumboardApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
